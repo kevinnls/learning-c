8a4412e1551342265f55650d88b007f99f2ad3e1
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-20 01:20:58 +0530

    trying to understand arrays and pointers

--- bubblesort/main.c
+++ bubblesort/main.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 
+int* sort();
+
 int main(){
-    if(printf("hello, world"))
-	return 0;
-    else
-	return 1;
+    int arr[] = {1,2,4,593,29,19,0};
+    sort(arr);
+}
+
+int* sort(int* arr){
+    printf("`arr` is: %d\n", arr);
+    printf("`*arr` is %d\n", *arr);
+    printf("`&arr` is %d\n", &arr);
+    printf("`arr[0]` is %d\n", arr[0]);
+    // printf("`*arr[0]` is %d\n", *arr[0]); /// not possible
+    printf("`&arr[0]` is %d\n", &arr[0]);
 }
