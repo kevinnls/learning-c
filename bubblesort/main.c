9b10e5df1e1e397b95445415df47a56d6790eb6f
kevinnls <57634663+kevinnls@users.noreply.github.com>
2021-09-20 01:47:28 +0530

    start fleshing out

--- bubblesort/main.c
+++ bubblesort/main.c
@@ -1,13 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int* sort(int*,int);
+int* sort(int*, int);
 
 int main(){
     int arr[] = {1,2,4,593,29,19,0};
-    sort(arr, sizeof(arr)/sizeof(arr[0]));
+    int arr_len = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr_len);
+    printf("\n");
+    return EXIT_SUCCESS;
 }
 
 int* sort(int* arr, int length){
-    printf("%d", length);
+
+    printf("length of array to be sorted: %d\n", length);
+    printf("elements of array to be sorted: ");
+    for(int i=0; i<length; i++){
+	if(i!=0) printf(", ");
+	printf("%d", arr[i]);
+    }
+
     return arr;
 }
